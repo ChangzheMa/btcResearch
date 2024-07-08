18f37e9677a5960992051521feaa277ee92f40a6
machangzhe <machangzhe@gmail.com>
2024-07-09 02:02:09 +0800

    初始化

---
+++ data/__init__.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+import os
+
+os.chdir('E:\\src\\btcResearch')
+
+
+def load_klines():
+    dfs = []
+    folder = 'binance/trade'
+    for file in os.listdir(folder):
+        if file.startswith('BTCFDUSD-1s-'):
+            dfs.append(pd.read_csv(os.path.join(folder, file),
+                                   names=['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time',
+                                          'quote_volume', 'count', 'taker_buy_volume', 'taker_buy_quote_volume',
+                                          'ignore']))
+    return pd.concat(dfs)
+
+
+def load_aggTrades():
+    dfs = []
+    folder = 'binance/trade'
+    for file in os.listdir(folder):
+        if file.startswith('BTCFDUSD-aggTrades-'):
+            dfs.append(pd.read_csv(os.path.join(folder, file),
+                                   names=['agg_trade_id', 'price', 'quantity', 'first_trade_id', 'last_trade_id',
+                                          'transact_time', 'is_buyer_maker', 'ignore']))
+    return pd.concat(dfs)
+
+
+def load_tickTrades():
+    dfs = []
+    folder = 'binance/trade'
+    for file in os.listdir(folder):
+        if file.startswith('BTCFDUSD-trades-'):
+            dfs.append(pd.read_csv(os.path.join(folder, file),
+                                   names=['id', 'price', 'qty', 'base_qty', 'time', 'is_buyer_maker', 'ignore']))
+    return pd.concat(dfs)
---
+++ main.py
@@ -0,0 +1,5 @@
+import data
+
+print(data.load_klines())
+print(data.load_aggTrades())
+print(data.load_tickTrades())
