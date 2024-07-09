35844eccb644cb06e2e04c69eee327e5b2f8d26a
machangzhe <machangzhe@gmail.com>
2024-07-10 02:25:04 +0800

    多策略回测

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import data
 import pandas as pd
-from strategy.price_momentum import ma5_ma10, ma5_ma10_ma20, ma5_ma10_ma20_revert
+from strategy.price_momentum import ma5_ma10, ma5_ma10_ma20, vwap5_vwap10_vwap20
 from backtest import do_backtest
 
 
 if __name__ == '__main__':
     df = data.load_klines()
-    df = ma5_ma10_ma20_revert(df)
+    df = ma5_ma10_ma20(df)
     do_backtest(df)
--- strategy/price_momentum.py
+++ strategy/price_momentum.py
@@ -30,6 +30,9 @@ def ma5_ma10_ma20(klines):
 
 
 def ma5_ma10_ma20_revert(klines):
+    """
+    这个不好，会亏本
+    """
     print("收盘价、5日均线、10日均线、20日均线 动量策略及反转")
     klines['MA5'] = klines['close'].rolling(window=5).mean()
     klines['MA10'] = klines['close'].rolling(window=10).mean()
@@ -40,6 +43,26 @@ def ma5_ma10_ma20_revert(klines):
 
     cond_down = (klines['close'] < klines['MA5']) & (klines['MA5'] < klines['MA10']) & (klines['MA10'] > klines['MA20'])
     cond_up = (klines['close'] > klines['MA5']) & (klines['MA5'] > klines['MA10']) & (klines['MA10'] < klines['MA20'])
+    klines['ops'] = 0
+    klines.loc[cond_down, 'ops'] = -1
+    klines.loc[cond_up, 'ops'] = 1
+    return klines
+
+
+def vwap5_vwap10_vwap20(klines):
+    print("收盘价、5日vwap、10日vwap、20日vwap 动量策略")
+    klines['typical_price'] = (klines['high'] + klines['low'] + klines['close']) / 3
+    klines['price_volume'] = klines['typical_price'] * klines['volume']
+
+    klines['vwap5'] = klines['price_volume'].rolling(window=5).sum() / klines['volume'].rolling(window=5).sum()
+    klines['vwap10'] = klines['price_volume'].rolling(window=10).sum() / klines['volume'].rolling(window=10).sum()
+    klines['vwap20'] = klines['price_volume'].rolling(window=20).sum() / klines['volume'].rolling(window=20).sum()
+    klines['vwap5'] = klines['vwap5'].fillna(method='bfill')
+    klines['vwap10'] = klines['vwap10'].fillna(method='bfill')
+    klines['vwap20'] = klines['vwap20'].fillna(method='bfill')
+
+    cond_down = (klines['close'] < klines['vwap5']) & (klines['vwap5'] < klines['vwap10']) & (klines['vwap10'] < klines['vwap20'])
+    cond_up = (klines['close'] > klines['vwap5']) & (klines['vwap5'] > klines['vwap10']) & (klines['vwap10'] > klines['vwap20'])
     klines['ops'] = 0
     klines.loc[cond_down, 'ops'] = -1
     klines.loc[cond_up, 'ops'] = 1
