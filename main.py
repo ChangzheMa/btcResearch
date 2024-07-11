a99256389ab8850707f4ef2ef83f39b143521eaa
machangzhe <machangzhe@gmail.com>
2024-07-11 10:58:15 +0800

    尝试一下带衰减的效果

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import data
 import pandas as pd
-from strategy.price_momentum import ma5_ma10, ma5_ma10_ma20, vwap5_vwap10_vwap20, ma5_ma10_ma20_choice
+from strategy.price_momentum import ma5_ma10, ma5_ma10_ma20, vwap5_vwap10_vwap20, ma5_ma10_ma20_choice, ma5_ma10_ma20_choice_and_decay
 from backtest import do_backtest
 import matplotlib.pyplot as plt
 
@@ -8,5 +8,5 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     period = "1s"
     df = data.load_klines(period)
-    df, label = ma5_ma10_ma20_choice(df, revert=False)
-    portfolios, day_portfolios = do_backtest(df, period, delay=1, label=label)
+    df, label = ma5_ma10_ma20_choice_and_decay(df, revert=False)
+    portfolios, day_portfolios = do_backtest(df, period, delay=3, label=label)
--- strategy/price_momentum.py
+++ strategy/price_momentum.py
@@ -107,3 +107,34 @@ def ma5_ma10_ma20_choice(klines, revert=False):
     if revert:
         klines['ops'] = -klines['ops']
     return klines, label
+
+
+def ma5_ma10_ma20_choice_and_decay(klines, revert=False):
+    decay = 0.7
+    label = f"收盘价、5日均线、10日均线、20日均线 动量策略，添加分钟线择时，{decay}衰减"
+    print(label)
+    klines['MA5'] = klines['close'].rolling(window=5).mean()
+    klines['MA10'] = klines['close'].rolling(window=10).mean()
+    klines['MA20'] = klines['close'].rolling(window=20).mean()
+    klines['MA5'] = klines['MA5'].fillna(method='bfill')
+    klines['MA10'] = klines['MA10'].fillna(method='bfill')
+    klines['MA20'] = klines['MA20'].fillna(method='bfill')
+
+    klines['MA300'] = klines['close'].rolling(window=300).mean()
+    klines['MA600'] = klines['close'].rolling(window=600).mean()
+    klines['MA300'] = klines['MA300'].fillna(method='bfill')
+    klines['MA600'] = klines['MA600'].fillna(method='bfill')
+
+    cond_down = (klines['close'] < klines['MA5']) & (klines['MA5'] < klines['MA10']) & (klines['MA10'] < klines['MA20'])
+    cond_up = (klines['close'] > klines['MA5']) & (klines['MA5'] > klines['MA10']) & (klines['MA10'] > klines['MA20'])
+    cond_down_reverse = ((klines['close'] < klines['MA300']) & (klines['MA300'] < klines['MA600']))
+    cond_up_reverse = (klines['close'] > klines['MA300']) & (klines['MA300'] > klines['MA600'])
+
+    klines['_ops'] = 0
+    klines.loc[cond_down & (~cond_down_reverse), '_ops'] = -1
+    klines.loc[cond_up & (~cond_up_reverse), '_ops'] = 1
+    klines['ops'] = klines['_ops'] * decay + klines['_ops'].shift(1) * (1-decay)
+    klines['ops'] = klines['ops'].fillna(method='bfill')
+    if revert:
+        klines['ops'] = -klines['ops']
+    return klines, label
